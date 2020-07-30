63d10fdd22f00f27a9434ceb58965501cbec391c
wentaiouyang <wentaiouyang@gmail.com>
2020-07-31 09:46:04 +1000

    result card icons

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import NavBar from "./components/NavBar/NavBar";
 import { BrowserRouter as Router } from "react-router-dom";
 import Routes from "./Routes/Router";
@@ -8,11 +8,12 @@ import { ToastProvider } from "react-toast-notifications";
 
 const SHEET_ID = "1p-CjJgmzIRmepETjpt7fJTPFiSypiebX-E0ZBUgUHX8";
 const ACCESS_TOKEN =
-  "ya29.a0AfH6SMD4XhKNhsBts2ku5ez4bqQ3BC08_HWdXQnDLREfmy6U3XB4B_z0wd_cClTAhJq2hIpXHvl2F4nf0mEj6f9_EKLDSNAn_lAxjlzASu5137kJRHmDnTXRd1Mf_uatT4AH0LcFtir8XFpOowfeJrS5JaL97i_zdJE";
+  "ya29.a0AfH6SMBVBZ047OGzS9eP6LD1_L2wMFkmdBY0H7swNhmnGpYz-t-NiiQCipJJxNQtk0twB_rbxLuTmQw7JrUQ9Yv90d68405yAzXieje78Ldv1krLNMrlnNwUgE9k1DMjZP67lxTxc9SAIPIhCgOkWhNaCcgqVFY0oEQ";
 
 function App() {
   const [state, setState] = useState({ user: [] });
   const [result, setResult] = useState("");
+  const user = useContext(AppContext);
   const getSheetValues = async () => {
     const request = await fetch(
       `https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/A:CM`,
@@ -29,16 +30,17 @@ function App() {
 
   useEffect(() => {
     getSheetValues();
+    console.log(localStorage.user);
   }, [result]);
 
   const authListener = () => {
     firebase.auth().onAuthStateChanged((user) => {
       if (user) {
         setState({ user });
-        //localStorage.setItem('user',user.uid);
+        localStorage.setItem("user", user.email);
       } else {
         setState({ user: null });
-        //localStorage.removeItem('user');
+        localStorage.removeItem("user");
       }
     });
   };
