51b63284542a0655753a43b535f1b84cc571d81c
zhijiang wang <wangzhijiang333@icloud.com>
2021-01-28 09:54:28 +1000

    added some questions

---
+++ src/answers/answers.js
@@ -0,0 +1,7 @@
+const answers = [
+
+  id = 0,
+  ans = 
+  Yes, students in all year levels receive 120 minutes of scheduled PE (not including sport) every week. 
+  
+]
--- src/containers/Survey.js
+++ src/containers/Survey.js
@@ -1,18 +1,88 @@
-import React from 'react';
-import classes from './Survey.module.scss';
-import HtmlForm from'../components/Form/HtmlForm';
+import React from 'react'
+import classes from './Survey.module.scss'
+import { useFormik } from 'formik'
 
-const Survey = ()=> {
+const Survey = () => {
+  const formik = useFormik({
+    initialValues: {
+      sex: '',
+      name: '',
+      age: ''
+    },
+    onSubmit: value => {
+      console.log(value)
+    }
+  })
+  return (
+    <body className={classes.container}>
+      <div className={classes.font}>
+        <form onSubmit={formik.handleSubmit}>
+          <h4>
+            HPE.1. Do students in all year levels receive 120 minutes of
+            scheduled PE (not including sport) every week?
+          </h4>
+          <input
+            {...formik.getFieldProps('sex')}
+            type="radio"
+            name="sex"
+            value="male"
+          />
+          Male
+          <input
+            {...formik.getFieldProps('sex')}
+            type="radio"
+            name="sex"
+            value="female"
+          />
+          Female
+          <h1>2</h1>
+          <input
+            {...formik.getFieldProps('name')}
+            type="radio"
+            name="name"
+            value="male"
+          />
+          Male
+          <input
+            {...formik.getFieldProps('name')}
+            type="radio"
+            name="name"
+            value="female"
+          />
+          Female
+          <h1>3</h1>
+          <input
+            {...formik.getFieldProps('age')}
+            type="radio"
+            name="age"
+            value="a"
+          />
+          a
+          <input
+            {...formik.getFieldProps('age')}
+            type="radio"
+            name="age"
+            value="b"
+          />
+          b
+          <input
+            {...formik.getFieldProps('age')}
+            type="radio"
+            name="age"
+            value="c"
+          />
+          c
+          <input
+            {...formik.getFieldProps('age')}
+            type="radio"
+            name="age"
+            value="d"
+          />
+          d<button type={'submit'}>Submit</button>
+        </form>
+      </div>
+    </body>
+  )
+}
 
-    return(
-
-        <body className={classes.container}>
-            <div className={classes.font}>
-            <HtmlForm/>
-            </div>
-        </body>
-        
-    )
-} 
-
-export default Survey;
+export default Survey
